78bbd5238ac14a620bdc3b4fae614adb4d5dc879
Burke Livingston <burkel24@gmail.com>
2023-12-22 10:18:33 -0800

    upgrade to shared eslint config

--- src/developers/dto/update-developer.dto.ts
+++ src/developers/dto/update-developer.dto.ts
@@ -1,8 +1,16 @@
+/* Copyright © Time By Ping, Inc. 2023. All rights reserved.
+ *
+ * Any unauthorized reproduction, distribution, public display, public
+ * performance or derivatization thereof can constitute, among other things, an
+ * infringement of Time By Ping Inc.’s exclusive rights under the Copyright Law
+ * of the U.S. (17 U.S.C. § 106) and may subject the infringer thereof to
+ * severe legal liability.*/
 import { PartialType } from '@nestjs/mapped-types';
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsString, ValidateNested } from 'class-validator';
 import { ObjectId } from 'mongodb';
+
 import { CreateDeveloperDto } from './create-developer.dto';
 
 export class UpdateDeveloperDto extends PartialType(CreateDeveloperDto) {
